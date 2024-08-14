c67cd1b96b8602c9a03fe7dc5b65bdebc77baa9f
Jose Laruta <eduardo.laruta@gmail.com>
2024-08-14 08:50:36 -0400

    scale rotation

--- 5.transformation_matrices/homogeneous.py
+++ 5.transformation_matrices/homogeneous.py
@@ -24,11 +24,58 @@ class MyWindow(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         self.draw_shape(self.vertices)
-        new = self.translate(self.vertices, 50, 50)
+        # print(self.vertices, self.get_center(self.vertices))
+        new = self.scale(self.vertices, 2.5)
         self.draw_shape(new)
 
     def translate(self, vertices, dx, dy):
         TM = np.array([[1, 0, dx], [0, 1, dy], [0, 0, 1]])
+        return self.apply_transform(vertices, TM)
+    
+    def rotate(self, vertices, angle):
+        angle_rad = np.radians(angle)
+        xc, yc = self.get_center(vertices)
+        # mover al origen
+        T1 = np.array([
+            [1, 0, -xc],
+            [0, 1, -yc],
+            [0, 0, 1]
+        ])
+        TM = np.array([
+            [np.cos(angle_rad), -np.sin(angle_rad), 0],
+            [np.sin(angle_rad), np.cos(angle_rad), 1],
+            [0, 0, 1],
+        ])
+        T2 = np.array([
+            [1, 0, xc],
+            [0, 1, yc],
+            [0, 0, 1]
+        ])
+        new_TM = np.dot(T2, np.dot(TM, T1))
+        return self.apply_transform(vertices, new_TM)
+    
+    def scale(self, vertices, s):
+        xc, yc = self.get_center(vertices)
+        # mover al origen
+        T1 = np.array([
+            [1, 0, -xc],
+            [0, 1, -yc],
+            [0, 0, 1]
+        ])
+        TM = np.array([
+            [s, 0, 0],
+            [0, s, 0],
+            [0, 0, 1]
+        ])
+        T2 = np.array([
+            [1, 0, xc],
+            [0, 1, yc],
+            [0, 0, 1]
+        ])
+        new_TM = np.dot(T2, np.dot(TM, T1))
+        return self.apply_transform(vertices, new_TM)
+    
+    def apply_transform(self, vertices, TM):
         v_array = np.array([[x, y, 1] for x, y in vertices])
         v_array = np.transpose(v_array)
         # apply transformation
@@ -37,13 +84,9 @@ class MyWindow(arcade.Window):
         new_v_array = np.transpose(new_v_array)
         new_v_list = new_v_array.tolist()
         return new_v_list
-
-
-    def scale(self, vertices, s):
-        pass
-
-    def rotate(self, vertices, angle, px=0, py=0):
-        pass
+    
+    def get_center(self, vertices):
+        return np.mean(np.array(vertices), axis=0)
     
     def draw_shape(self, vertices):
         for x, y in vertices:
