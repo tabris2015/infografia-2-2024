61b462af97e892a3c429b021648b0d517aab6238
Jose Laruta <eduardo.laruta@gmail.com>
2024-08-14 08:15:32 -0400

    translate

--- 5.transformation_matrices/homogeneous.py
+++ 5.transformation_matrices/homogeneous.py
@@ -1,6 +1,7 @@
 import arcade
 import arcade.color
 import math
+import numpy as np
 
 # definicion de constantes
 SCREEN_WIDTH = 800
@@ -15,23 +16,35 @@ class MyWindow(arcade.Window):
         self.vertices = [(100, 100), (100, 200), (200, 200), (200, 100)]
         
     def on_update(self, delta_time: float):
-        print(delta_time)
+        pass
+        # print(delta_time)
         # self.vertices = self.rotate(self.vertices, 10 * delta_time, 200, 200)
         # self.vertices = self.translate(self.vertices, 10 * delta_time, 10 * delta_time)
         
     def on_draw(self):
         arcade.start_render()
         self.draw_shape(self.vertices)
+        new = self.translate(self.vertices, 50, 50)
+        self.draw_shape(new)
 
     def translate(self, vertices, dx, dy):
+        TM = np.array([[1, 0, dx], [0, 1, dy], [0, 0, 1]])
+        v_array = np.array([[x, y, 1] for x, y in vertices])
+        v_array = np.transpose(v_array)
+        # apply transformation
+        new_v_array = np.dot(TM, v_array)
+        new_v_array = new_v_array[:2, :]
+        new_v_array = np.transpose(new_v_array)
+        new_v_list = new_v_array.tolist()
+        return new_v_list
+
+
+    def scale(self, vertices, s):
         pass
 
     def rotate(self, vertices, angle, px=0, py=0):
         pass
     
-    def scale(self, vertices, s):
-        pass
-
     def draw_shape(self, vertices):
         for x, y in vertices:
             arcade.draw_point(x, y, arcade.color.RED, 4)
