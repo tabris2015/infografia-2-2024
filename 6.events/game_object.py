9f057a969e575e6113dbd981089f4b743643b347
Jose Laruta <eduardo.laruta@gmail.com>
2024-08-15 08:41:45 -0400

    tank movement

--- 6.events/game_object.py
+++ 6.events/game_object.py
@@ -77,3 +77,33 @@ class Polygon2D:
 
     def draw(self):
         arcade.draw_polygon_outline(self.vertices, self.color, 5)
+
+
+class Tank:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.speed = 0
+        self.angular_speed = 0
+        self.theta = 0
+        self.body = Polygon2D(
+            [(100 + x, y), (x, 50 +y), (x, -50 + y)],
+            color
+        )
+
+    def update(self, delta_time: float):
+        dtheta = self.angular_speed * delta_time
+        dx = self.speed * math.cos(self.theta) * delta_time
+        dy = self.speed * math.sin(self.theta) * delta_time
+
+        # apply transforms
+        self.body.translate(dx, dy)
+        self.body.rotate(dtheta, pivot=(self.x, self.y))
+
+        # update tank state
+        self.theta += dtheta
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        self.body.draw()
---
+++ 6.events/tank_game.py
@@ -0,0 +1,54 @@
+import arcade
+import random
+from game_object import Tank
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Polygon2d"
+
+SPEED = 40
+ANGULAR_SPEED = 1.5
+
+def get_random_color():
+    return (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+    )
+
+class App(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.BLACK)
+        self.tank = Tank(400, 400, get_random_color())
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.UP:
+            self.tank.speed = SPEED
+        if symbol == arcade.key.DOWN:
+            self.tank.speed = -SPEED
+        
+        if symbol == arcade.key.LEFT:
+            self.tank.angular_speed = ANGULAR_SPEED
+        if symbol == arcade.key.RIGHT:
+            self.tank.angular_speed = -ANGULAR_SPEED
+        
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.UP or symbol == arcade.key.DOWN:
+            self.tank.speed = 0
+        
+        if symbol == arcade.key.LEFT or symbol == arcade.key.RIGHT:
+            self.tank.angular_speed = 0
+            
+    def on_update(self, delta_time: float):
+        self.tank.update(delta_time)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.tank.draw()
+
+
+if __name__ == "__main__":
+    app = App()
+    arcade.run()
