b07354c401fcad257a18d00d1faa22d4a2aeacdc
Jose Laruta <eduardo.laruta@gmail.com>
2024-08-20 08:42:05 -0400

    shapes

--- 9.pymunk/car.py
+++ 9.pymunk/car.py
@@ -19,19 +19,53 @@ class Car:
         chassis_shape.elasticity = 0.3
         chassis_shape.friction = 0.5
 
-        space.add(chassis_body, chassis_shape)
+        # wheels
+        f_wheel_body = pymunk.Body()
+        f_wheel_body.position = (x + 50, y - 35)
+        f_wheel_shape = pymunk.Circle(f_wheel_body, 20)
+        f_wheel_shape.density = 0.01
+        f_wheel_shape.friction = 0.5
+        f_wheel_shape.elasticity = 1
+        self.f_wheel_sprite = arcade.SpriteCircle(20, arcade.color.GREEN)
+        
+        r_wheel_body = pymunk.Body()
+        r_wheel_body.position = (x - 50, y - 35)
+        r_wheel_shape = pymunk.Circle(r_wheel_body, 20)
+        r_wheel_shape.density = 0.01
+        r_wheel_shape.friction = 0.5
+        r_wheel_shape.elasticity = 1
+        self.r_wheel_sprite = arcade.SpriteCircle(20, arcade.color.GREEN)
 
+
+        space.add(chassis_body, chassis_shape)
+        space.add(f_wheel_body, f_wheel_shape)
+        space.add(r_wheel_body, r_wheel_shape)
+        
         self.chassis_sprite = arcade.SpriteSolidColor(100, 70, arcade.color.RED)
 
         self.sprites = arcade.SpriteList()
         self.sprites.append(self.chassis_sprite)
+        self.sprites.append(self.f_wheel_sprite)
+        self.sprites.append(self.r_wheel_sprite)
         self.chassis_body = chassis_body
         self.chassis_shape = chassis_shape
+        self.f_wheel = f_wheel_shape
+        self.r_wheel = r_wheel_shape
+        
     
     def update(self):
         self.chassis_sprite.center_x = self.chassis_shape.body.position.x
-        self.chassis_sprite.center_x = self.chassis_shape.body.position.y
+        self.chassis_sprite.center_y = self.chassis_shape.body.position.y
         self.chassis_sprite.radians = self.chassis_shape.body.angle
+
+        self.f_wheel_sprite.center_x = self.f_wheel.body.position.x
+        self.f_wheel_sprite.center_y = self.f_wheel.body.position.y
+        self.f_wheel_sprite.radians = self.f_wheel.body.angle
+
+        self.r_wheel_sprite.center_x = self.r_wheel.body.position.x
+        self.r_wheel_sprite.center_y = self.r_wheel.body.position.y
+        self.r_wheel_sprite.radians = self.r_wheel.body.angle
+
 
 
 class App(arcade.Window):
@@ -42,8 +76,9 @@ class App(arcade.Window):
         self.space = pymunk.Space()
         self.space.gravity = (0, -900)
 
-        self.car = Car(700, 400, self.space)
-
+        self.car = Car(200, 50, self.space)
+        self.lines = []
+        self.add_static()
         # sprites
         self.sprites = arcade.SpriteList() 
         self.sprites.extend(self.car.sprites)
@@ -56,6 +91,18 @@ class App(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         self.sprites.draw()
+
+    def add_static_segment(self, x0, y0, x1, y1):
+        segment_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        segment_shape = pymunk.Segment(segment_body, [x0, y0], [x1, y1], 0)
+        segment_shape.friction = 0.1
+        self.space.add(segment_body, segment_shape)
+        self.lines.append((x0, y0, x1, y1))
+
+    def add_static(self):
+        # agregar piso
+        self.add_static_segment(0, 0, WIDTH, 0)
+
 
 
 if __name__ == "__main__":
