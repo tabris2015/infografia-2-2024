d7c5b4aa56bcfff723eea2cd4cc509e2ffe4ea2c
Jose Laruta <eduardo.laruta@gmail.com>
2024-08-28 08:28:07 -0400

    attack anim

--- 10.godot/fox/scripts/player.gd
+++ 10.godot/fox/scripts/player.gd
@@ -4,11 +4,30 @@ const ACCELERATION = 500
 const FRICTION = 500
 const MAX_SPEED = 100
 
+# estados del player
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+# variable de estado actual
+var state = MOVE
+
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = $AnimationTree.get("parameters/playback")
 
 
 func _physics_process(delta: float) -> void:
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_axis("ui_left", "ui_right")
 	input_vector.y = Input.get_axis("ui_up", "ui_down")
@@ -17,6 +36,7 @@ func _physics_process(delta: float) -> void:
 	if input_vector != Vector2.ZERO:
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_tree.set("parameters/Attack/blend_position", input_vector)
 		state_machine.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -24,3 +44,14 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("attack"):
+		# transicion a ATTACK
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	state_machine.travel("Attack")
+
+func attack_anim_finished():
+	state = MOVE
